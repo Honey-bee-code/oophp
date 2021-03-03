c4bdcbf5c882b7658b448a3a9b4103cfffddca5c
Honey-bee-code <hanifinida@gmail.com>
2021-03-03 10:25:06 +0800

    object type

---
+++ object_type.php
@@ -0,0 +1,41 @@
+<?php
+
+class Produk {
+    public  $judul,
+            $penulis,
+            $penerbit,
+            $harga;
+    
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getlabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+
+class CetakInfoProduk {
+    public function cetak(Produk $produk){
+        $str = "{$produk->judul} | {$produk->getlabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+
+$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000);
+
+$produk3 = new Produk("Dragon Ball");
+
+echo "Komik : " . $produk1->getlabel();
+echo "<br>";
+echo "Game : " . $produk2->getlabel();
+echo "<br>";
+
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk3);
