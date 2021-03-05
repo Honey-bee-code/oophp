8ca711296b9a7e195ecbd9bcf881d9f25b84c510
Honey-bee-code <hanifinida@gmail.com>
2021-03-05 21:57:26 +0800

    namespace

--- namespace/App/init.php
+++ namespace/App/init.php
@@ -5,8 +5,20 @@
 // require_once 'Produk/Komik.php';
 // require_once 'Produk/Game.php';
 // require_once 'Produk/CetakInfoProduk.php';
+// require_once 'Produk/User.php';
+
+// require_once 'Service/User.php';
 
 
 spl_autoload_register(function($class){
-    require_once 'Produk/' .$class. '.php';
+    // App\Produk\User = ["App", "Produk", "Class"]
+    $class = explode('\\', $class);
+    $class = end($class); // untuk mengambil elemen terakhir
+    require_once __DIR__.'/Produk/' .$class. '.php';
+});
+
+spl_autoload_register(function($class){
+    $class = explode('\\', $class);
+    $class = end($class);
+    require_once __DIR__.'/Service/' .$class. '.php';
 });
--- namespace/index.php
+++ namespace/index.php
@@ -3,16 +3,22 @@
 require_once 'App/init.php';
 
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 
-$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+// $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk($produk1);
-$cetakProduk->tambahProduk($produk2);
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
 
-echo "<hr>";
+// echo "<hr>";
 
-new Coba();
 
+// memberi alias untuk namespace
+use App\Produk\User as ProdukUser;
+use App\Service\User;
+
+new ProdukUser();
+echo "<br>";
+new User();
